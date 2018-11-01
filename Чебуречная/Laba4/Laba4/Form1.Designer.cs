a8012895a0e8a1dc37b799ce533433bb59072d3e
Виталий Паршаков <vaparshakov@edu.hse.ru>
2018-11-01 20:58:42 +0500

    kek

---
+++ Чебуречная/Laba4/Laba4/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Laba4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Mas = new System.Windows.Forms.DataGridView();
+            this.Time_1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time_2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Mas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Mas
+            // 
+            this.Mas.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle1.Format = "N2";
+            dataGridViewCellStyle1.NullValue = null;
+            this.Mas.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.Mas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Mas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Mas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Time_1,
+            this.Time_2});
+            this.Mas.Location = new System.Drawing.Point(12, 12);
+            this.Mas.Name = "Mas";
+            this.Mas.Size = new System.Drawing.Size(251, 426);
+            this.Mas.TabIndex = 0;
+            // 
+            // Time_1
+            // 
+            this.Time_1.HeaderText = "Time_1";
+            this.Time_1.Name = "Time_1";
+            // 
+            // Time_2
+            // 
+            this.Time_2.HeaderText = "Time_2";
+            this.Time_2.Name = "Time_2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Mas);
+            this.Name = "Form1";
+            this.Text = "Алгоритм Джонсона";
+            ((System.ComponentModel.ISupportInitialize)(this.Mas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Mas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time_1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time_2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Чебуречная/Laba4/Laba4/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<List<int>> mas = new List<List<int>>();
+            int[][] mas2 = new int[Mas.Rows.Count-1][];
+            int c = 0;
+            foreach (DataGridViewRow row in Mas.Rows)
+            {
+                if(c!= Mas.Rows.Count - 1)
+                {
+                    mas2[c] = new int[3];
+                    mas.Add(new List<int>());
+                    mas[c].Add(Convert.ToInt32(row.Cells[0].Value));
+                    mas[c].Add(Convert.ToInt32(row.Cells[1].Value));
+                    mas[c].Add(c++);
+                }
+            }
+            c = 0;
+            int min,right=mas2.Length-1;
+            for (int i = 0; i < mas.Count; )
+            {
+                int s = 0,g=0;
+                min = mas[i][0];
+                if (mas[i][1] < min) { min = mas[i][1];g = 1; }
+                for (int j = 1; j < mas.Count; j++)
+                {
+                    if (mas[j][0] < min) { min = mas[j][0]; s = j; g = 0; }
+                    if (mas[j][1] < min) { min = mas[j][1]; s = j; g = 1; }
+                }
+                if (g == 0)
+                {
+                    mas2[c][0] = mas[s][0];
+                    mas2[c][1] = mas[s][1];
+                    mas2[c++][2] = mas[s][2];
+                }
+                else
+                {
+                    mas2[right][0] = mas[s][0];
+                    mas2[right][1] = mas[s][1];
+                    mas2[right--][2] = mas[s][2];
+                }
+                min = mas[i][1];
+                mas.RemoveAt(s);
+            }
+            MessageBox.Show("Kek");
+        }
+    }
+}
